08f63bad6e598669daf209ad1c1cfd040fb60af1
Pedro Victor <80000694+PGurgel@users.noreply.github.com>
2022-05-18 02:00:20 -0300

    Add files via upload

--- script.js
+++ script.js
@@ -13,3 +13,21 @@ const enviarFormulario = () => {
     var win = window.open(`https://wa.me/${numero}?text=Nome:%20${nome}%0DBairro:%20${bairro}%0DRua:%20${endereco}%0DPedido:%20${pizza},%20${borda},%20${tamanho}%0D${bebidas}%0D${hamburguer}%0DPagamento:%20${pagamento}`, '_blank');
 }
 evento.addEventListener('click', enviarFormulario);
+
+var btnPizza = document.getElementById("opcPizza");
+var btnHamburger = document.getElementById("opcHamburguer");
+var btnBebidaSuco = document.getElementById("opcBebidaSuco");
+
+btnPizza.addEventListener('click', function(){
+    document.getElementById('pizzas').style.display = 'grid';
+    document.getElementById('tamanhos').style.display = 'grid';
+    document.getElementById('bordas').style.display = 'grid';
+
+})
+btnHamburger.addEventListener('click',function(){
+    document.getElementById('hamburgueres').style.display = 'grid';
+} )
+
+btnBebidaSuco.addEventListener('click',function(){
+    document.getElementById('bebidasSucos').style.display = 'grid';
+} )
