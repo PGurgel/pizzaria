dd83dccff1a85c01e4c5e4ffd9a41653a92c308a
Pedro Victor <80000694+PGurgel@users.noreply.github.com>
2022-05-20 22:24:22 -0300

    Add files via upload

---
+++ scripts/addValue.js
@@ -0,0 +1,8 @@
+                     /* declarar variaveis */
+let pizzaCalabresa = document.getElementById('pizzaCalabresa');
+let pizzaFrango = document.getElementById('pizzaFrango');
+                      /* atribuir valores */
+pizzaCalabresa = 30;
+pizzaFrango = 30;
+                      /* mostrar na tela */
+/*document.getElementById('total').innerHTML = pizzaCalabresa + pizzaFrango;*/
---
+++ scripts/switch.js
@@ -0,0 +1,40 @@
+var btnPizza = document.getElementById("opcPizza");
+var btnHamburger = document.getElementById("opcHamburguer");
+var btnBebidaSuco = document.getElementById("opcBebidaSuco");
+var btnGuloseima = document.getElementById('opcGuloseima');
+
+btnPizza.addEventListener('click', function () {
+    if (document.getElementById('pizzas').style.display === 'grid' && document.getElementById('tamanhos').style.display === 'grid' && document.getElementById('bordas').style.display === 'grid') {
+        document.getElementById('pizzas').style.display = 'none';
+        document.getElementById('tamanhos').style.display = 'none';
+        document.getElementById('bordas').style.display = 'none';
+    } else {
+        document.getElementById('pizzas').style.display = 'grid';
+        document.getElementById('tamanhos').style.display = 'grid';
+        document.getElementById('bordas').style.display = 'grid';
+    }
+
+})
+btnHamburger.addEventListener('click', function () {
+  if( document.getElementById('hamburgueres').style.display === 'grid'){
+    document.getElementById('hamburgueres').style.display = 'none';
+  }else{
+    document.getElementById('hamburgueres').style.display = 'grid';
+  }
+})
+
+btnBebidaSuco.addEventListener('click', function () {
+   if(document.getElementById('bebidasSucos').style.display === 'grid'){
+    document.getElementById('bebidasSucos').style.display = 'none';
+   }else{
+    document.getElementById('bebidasSucos').style.display = 'grid';
+   }
+})
+
+btnGuloseima.addEventListener('click', function () {
+  if( document.getElementById('guloseimas').style.display === 'grid'){
+    document.getElementById('guloseimas').style.display = 'none';
+  }else{
+    document.getElementById('guloseimas').style.display = 'grid';
+  }
+})
