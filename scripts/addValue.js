5cf4869231e379ac5e72ff2e9549ddbda061e311
Pedro Victor <pgurgelbezerra@gmail.com>
2022-10-09 13:58:04 -0300

    add de condicionais

--- scripts/addValue.js
+++ scripts/addValue.js
@@ -3,16 +3,36 @@ let tamanho = document.querySelector("#tamanho");
 
 const PizzaEvent = document.querySelector(".tudo");
 PizzaEvent.addEventListener('click', () => {
-  if (pizza.value != "Pizza de Carne de sol" && tamanho.value == "Tamanho P") {
-    document.getElementById('total').innerHTML = 17;
-  } else if (pizza.value != "Pizza de Carne de sol" && tamanho.value == "Tamanho M") {
-    document.getElementById('total').innerHTML = 23;
-  } else if (pizza.value != "Pizza de Carne de sol" && tamanho.value == "Tamanho G") {
-    document.getElementById('total').innerHTML = 28;
-  }else if (pizza.value == "Pizza de Carne de sol" && tamanho.value == "Tamanho P") {
-    document.getElementById('total').innerHTML = 19;
+  if (pizza.value != "Pizza de Carne de sol" && pizza.value != "Pizza de Carne de sol com Catupiry" && pizza.value != "Pizza doce - chocolate e Cartola" && tamanho.value == "Tamanho P" && borda.value == "Borda: Sem borda") {
+    document.getElementById('total').innerHTML = "$"+ 17;
+  } else if (pizza.value != "Pizza de Carne de sol" && pizza.value != "Pizza de Carne de sol com Catupiry" && pizza.value != "Pizza doce - chocolate e Cartola" && tamanho.value == "Tamanho M" && borda.value == "Borda: Sem borda") {
+    document.getElementById('total').innerHTML = "$"+ 23;
+  } else if (pizza.value != "Pizza de Carne de sol" && pizza.value != "Pizza de Carne de sol com Catupiry" && pizza.value != "Pizza doce - chocolate e Cartola" && tamanho.value == "Tamanho G" && borda.value == "Borda: Sem borda") {
+    document.getElementById('total').innerHTML = "$"+ 28;
+
+  }else if (pizza.value != "Pizza de Carne de sol" && pizza.value != "Pizza de Carne de sol com Catupiry" && pizza.value != "Pizza doce - chocolate e Cartola" && tamanho.value == "Tamanho P" && borda.value == "Borda: Chocolate" && borda.value == "Borda: Catupiry") {
+    document.getElementById('total').innerHTML = "$"+ 17;
+  }else if (pizza.value != "Pizza de Carne de sol" && pizza.value != "Pizza de Carne de sol com Catupiry" && pizza.value != "Pizza doce - chocolate e Cartola" && tamanho.value == "Tamanho M" && borda.value == "Borda: Chocolate" && borda.value == "Borda: Catupiry") {
+      document.getElementById('total').innerHTML = "$"+ 25;
+  }else if (pizza.value != "Pizza de Carne de sol" && pizza.value != "Pizza de Carne de sol com Catupiry" && pizza.value != "Pizza doce - chocolate e Cartola" && tamanho.value == "Tamanho G" && borda.value == "Borda: Chocolate" && borda.value == "Borda: Catupiry") {
+      document.getElementById('total').innerHTML = "$"+ 30;
+    
+
+  }else if (pizza.value == "Pizza de Carne de sol" && pizza.value == "Pizza de Carne de sol com Catupiry" && pizza.value == "Pizza doce - chocolate e Cartola" && tamanho.value == "Tamanho P" && borda.value == "Borda: Sem borda") {
+    document.getElementById('total').innerHTML = "$"+ 19;
+  }else if (pizza.value == "Pizza de Carne de sol" && pizza.value == "Pizza de Carne de sol com Catupiry" && pizza.value == "Pizza doce - chocolate e Cartola" && tamanho.value == "Tamanho M" && borda.value == "Borda: Sem borda") {
+    document.getElementById('total').innerHTML = "$"+ 26;
+  }else if (pizza.value == "Pizza de Carne de sol" && pizza.value == "Pizza de Carne de sol com Catupiry" && pizza.value == "Pizza doce - chocolate e Cartola" && tamanho.value == "Tamanho G" && borda.value == "Borda: Sem borda") {
+    document.getElementById('total').innerHTML = "$"+ 31;
   }
-   else {
-    document.getElementById('total').innerHTML = 0;
+  else if (pizza.value == "Pizza de Carne de sol" && pizza.value == "Pizza de Carne de sol com Catupiry" && pizza.value == "Pizza doce - chocolate e Cartola" && tamanho.value == "Tamanho P" && borda.value == "Borda: Chocolate" && borda.value == "Borda: Catupiry") {
+  document.getElementById('total').innerHTML = "$"+ 19;
+  }else if (pizza.value == "Pizza de Carne de sol" && pizza.value == "Pizza de Carne de sol com Catupiry" && pizza.value == "Pizza doce - chocolate e Cartola" && tamanho.value == "Tamanho M" && borda.value == "Borda: Chocolate" && borda.value == "Borda: Catupiry") {
+  document.getElementById('total').innerHTML = "$"+ 26;
+  }else if (pizza.value == "Pizza de Carne de sol" && pizza.value == "Pizza de Carne de sol com Catupiry" && pizza.value == "Pizza doce - chocolate e Cartola" && tamanho.value == "Tamanho G" && borda.value == "Borda: Chocolate" && borda.value == "Borda: Catupiry") {
+  document.getElementById('total').innerHTML = "$"+ 31;
+
+  }else {
+    document.getElementById('total').innerHTML ="$"+ 0;
   }
 });
