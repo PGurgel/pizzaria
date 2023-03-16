6d9fa97a59db8369472c0b3d4591084cbb7fb4dd
Pedro Victor <pgurgelbezerra@gmail.com>
2023-03-16 18:57:44 -0300

    feat: add quantidades

--- scripts/addValue.js
+++ scripts/addValue.js
@@ -1,11 +1,13 @@
-let pizza = document.querySelector("#pizza");
-let tamanho = document.querySelector("#tamanho");
-let borda = document.querySelector("#borda");
-let preco = document.getElementById('total');
+  let pizza = document.querySelector("#pizza");
+  let tamanho = document.querySelector("#tamanho");
+  let borda = document.querySelector("#borda");
+  let qnts = document.querySelector("#qnt");
+  let preco = document.getElementById('total');
 
 const PizzaEvent = document.querySelector(".tudo");
- 
+
 PizzaEvent.addEventListener('click', () => {
-  preco.innerHTML =(+pizza.value)+(+tamanho.value)+(+borda.value);
+ 
+  preco.innerHTML = ((+pizza.value)+(+tamanho.value)+(+borda.value))*(qnts.value);
 });
 
--- scripts/form.js
+++ scripts/form.js
@@ -3,7 +3,7 @@ const enviarFormulario = () => {
     let nome = document.getElementById('nome').value;
     let bairro = document.getElementById('bairro').value;
     let endereco = document.getElementById('enderco').value;
-    let pizza = document.getElementById('pizza').name;
+    let pizza = document.getElementById('pizza');
     let tamanho = document.getElementById('tamanho').name;
     let borda = document.getElementById('borda').name;
     let hamburguer = document.getElementById('hamburguer').value;
@@ -11,6 +11,6 @@ const enviarFormulario = () => {
     let pagamento = document.getElementById('pagamento').value;
     let valor = document.getElementById('total').innerHTML;
     let numero = 558499150415;
-var win = window.open(`https://wa.me/${numero}?text=Nome:%20${nome}%0DBairro:%20${bairro}%0DRua:%20${endereco}%0DPedido:%20${pizza}%20${borda}%20${tamanho}%0D${bebidas}%0D${hamburguer}%0DPagamento:%20${pagamento}%0DValor:%20$${valor}`,'_blank');
+var win = window.open(`https://wa.me/${numero}?text=Nome:%20${nome}%0DBairro:%20${bairro}%0DRua:%20${endereco}%0DPedido:%20${pizza.id}%20${borda}%20${tamanho}%0D${bebidas}%0D${hamburguer}%0DPagamento:%20${pagamento}%0DValor:%20$${valor}`,'_blank');
 }
 evento.addEventListener('click', enviarFormulario);
