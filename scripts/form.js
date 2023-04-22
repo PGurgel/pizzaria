88df2d4a1b28823737734eb2e839b1bea3dce2bb
Pedro Victor <pgurgelbezerra@gmail.com>
2023-04-22 15:06:50 -0300

    add ajustes no forms

--- scripts/form.js
+++ scripts/form.js
@@ -1,16 +1,26 @@
 const evento = document.getElementById('enviar');
 const enviarFormulario = () => {
+
+    let pizza = document.getElementById("pizza");
+    let opcaoPizza = pizza.options[pizza.selectedIndex].innerHTML;
+   
+    let tamanho = document.getElementById("tamanho");
+    let opcaoTamanho = tamanho.options[tamanho.selectedIndex].innerHTML;
+
+    let borda = document.getElementById("borda");
+    let opcaoBorda = borda.options[borda.selectedIndex].innerHTML;
+
     let nome = document.getElementById('nome').value;
     let bairro = document.getElementById('bairro').value;
     let endereco = document.getElementById('enderco').value;
-    let pizza = document.getElementById('pizza');
-    let tamanho = document.getElementById('tamanho').name;
-    let borda = document.getElementById('borda').name;
+   
+    
     let hamburguer = document.getElementById('hamburguer').value;
     let bebidas = document.getElementById('bebidas').value;
     let pagamento = document.getElementById('pagamento').value;
     let valor = document.getElementById('total').innerHTML;
     let numero = 558499150415;
-var win = window.open(`https://wa.me/${numero}?text=Nome:%20${nome}%0DBairro:%20${bairro}%0DRua:%20${endereco}%0DPedido:%20${pizza.id}%20${borda}%20${tamanho}%0D${bebidas}%0D${hamburguer}%0DPagamento:%20${pagamento}%0DValor:%20$${valor}`,'_blank');
+var win = window.open(`https://wa.me/${numero}?text=Nome:%20${nome}.%0DBairro:%20${bairro}.%0DRua:%20${endereco}.%0DPedido:%20${opcaoPizza}%20${opcaoBorda}.%20${opcaoTamanho}%0D${bebidas}.%0D${hamburguer}.%0DPagamento:%20${pagamento}%0DValor:%20$${valor}`,'_blank');
 }
 evento.addEventListener('click', enviarFormulario);
+
