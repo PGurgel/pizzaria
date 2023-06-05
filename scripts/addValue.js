50a918a9de26b1a6ea5272a147af9e66aeb41940
Pedro Victor <pgurgelbezerra@gmail.com>
2023-06-05 14:16:10 -0300

    feat: reset

--- scripts/addValue.js
+++ scripts/addValue.js
@@ -1,26 +1,14 @@
-  let pizza = document.querySelector("#pizza");
-  let tamanho = document.querySelector("#tamanho");
-  let borda = document.querySelector("#borda");
-  let qnts = document.querySelector("#qnt");
-  let precoPizza= document.getElementById('total');
-  let hamburguer = document.querySelector('#hamburguer');
+let pizza = document.querySelector("#pizza");
+let tamanho = document.querySelector("#tamanho");
+let borda = document.querySelector("#borda");
+let qnts = document.querySelector("#qnt");
+let total = document.getElementById('total');
+let hamburguer = document.querySelector('#hamburguer');
 
 const PizzaEvent = document.querySelector(".tudo");
 
 PizzaEvent.addEventListener('click', () => {
 
-  precoPizza.innerHTML = ((+pizza.value)+(+tamanho.value)+(+borda.value))*(qnts.value);
-  
- 
-});
+  total.innerHTML = ((+pizza.value) + (+tamanho.value) + (+borda.value)) * (qnts.value)
 
-let precoHamburger= document.getElementById('total');
-
-const hamburguerEvent = document.querySelector(".tudo");
-
-hamburguerEvent.addEventListener('click', () => {
-
-  precoHamburger.innerHTML = ((+hamburguer.value));
-  
- 
 });
